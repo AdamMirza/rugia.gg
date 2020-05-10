d918e2f228f1d376c7eb461d319308ab7eaf0b16
Adam Mirza <adammirza12@hotmail.com>
2020-05-10 01:11:25 -0700

    Added description and list (list not working right yet)

--- src/app/pokemon-team-icon/pokemon-team-icon.component.ts
+++ src/app/pokemon-team-icon/pokemon-team-icon.component.ts
@@ -12,7 +12,7 @@ export class PokemonTeamIconComponent implements OnInit {
 
   constructor(private pds: PokemonDetailService) {
     this.userPokemonTeam.push(new UserPokemon('Bulbasaur', 'https://art.pixilart.com/6ba93206eb9dad5.png', 60));
-    this.userPokemonTeam.push(new UserPokemon('Charmander', 'https://art.pixilart.com/6ba93206eb9dad5.png', 60));
+    this.userPokemonTeam.push(new UserPokemon('Charmander', 'https://lh3.googleusercontent.com/proxy/2z_2b-uqgw-VEFrzJwbj5Gxu-V2u_Q4fL7-OdEkhft34WD8j0U_IQIXbz4Arr4b9jrbZdeX1kAAzi5Rn7KRq4aSqkZK2RD54', 60));
     this.userPokemonTeam.push(new UserPokemon('Bulbasaur', 'https://art.pixilart.com/6ba93206eb9dad5.png', 60));
     this.userPokemonTeam.push(new UserPokemon('Bulbasaur', 'https://art.pixilart.com/6ba93206eb9dad5.png', 60));
     this.userPokemonTeam.push(new UserPokemon('Bulbasaur', 'https://art.pixilart.com/6ba93206eb9dad5.png', 60));
@@ -24,7 +24,6 @@ export class PokemonTeamIconComponent implements OnInit {
   }
 
   updateCurrentPokemon(userPokemon: UserPokemon) {
-    console.log(userPokemon);
     this.pds.setCurrentPokemon(userPokemon);
   }
 
@@ -38,6 +37,7 @@ export class UserPokemon {
   current_hp: number;
   xp_to_next_lvl: number;
   current_xp: number;
+  stats: EvIvList;
 
   constructor(name: string, url: string, level: number) {
     this.name = name;
@@ -47,5 +47,58 @@ export class UserPokemon {
     this.current_hp = 80;
     this.xp_to_next_lvl = 200;
     this.current_xp = 62;
+    this.stats = new EvIvList(
+      new EvIvStat(StatName.MaxHp, 10, 10, 10),
+      new EvIvStat(StatName.Atk, 20, 20, 20),
+      new EvIvStat(StatName.Def, 30, 30, 30),
+      new EvIvStat(StatName.SpAtk, 40, 40, 40),
+      new EvIvStat(StatName.SpDef, 50, 50, 50),
+      new EvIvStat(StatName.Speed, 60, 60, 60)
+    );
   }
+
+  getStatsList() {
+    return this.stats.statsList;
+  }
+}
+
+export class EvIvList {
+  statsList: EvIvStat[];
+  
+  constructor(max_hp: EvIvStat, atk: EvIvStat, def: EvIvStat, spAtk: EvIvStat, spDef: EvIvStat, speed: EvIvStat) {
+    this.statsList = new Array(6);
+    console.log(max_hp);
+    this.statsList.push(speed);
+    this.statsList.push(spDef);
+    this.statsList.push(spAtk);
+    this.statsList.push(def);
+    this.statsList.push(atk);
+    this.statsList.push(max_hp);
+  }
+}
+
+export class EvIvStat {
+  statName: StatName;
+  base: number;
+  iv: number;
+  ev: number;
+  total: number;
+
+  constructor(statName: StatName, base: number, iv: number, ev: number) {
+    this.statName = statName;
+    this.base = base;
+    console.log(this.base);
+    this.iv = iv;
+    this.ev = ev;
+    this.total = base + iv + ev;
+  }
+}
+
+enum StatName {
+  MaxHp = "Max HP",
+  Atk = "Attack",
+  Def = "Defense",
+  SpAtk = "Special Attack",
+  SpDef = "Special Defense",
+  Speed = "Speed"
 }
